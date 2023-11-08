4f8b6731e5e8021fd847d1e2e978e8e1c99c5fe3
LeaSonnenberg <Lea.Sonnenberg@btc-ag.com>
2023-11-08 08:31:02 +0100

    Startpokemon auswählen anfang weiter

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -4,6 +4,7 @@ public class Game {
     public static void main (String[] args){
         UI ui = new ConsolenUI();
         Player player = new Player(ui.getUserInput("Wie heißt du? "));
+
         Pokemon pokemon = new Pokemon(ui.displayPokemon("Wähle ein Pokemon aus: "));
         player.pokemonName();
     }
